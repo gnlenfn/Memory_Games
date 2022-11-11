d5df786139d8f1da327f52c11c8f0eb75920baa5
Yun, Hong In <akenfn513@gmail.com>
2022-11-11 17:43:57 +0900

    FIX: reset without reload

--- js/MemoryGame.js
+++ js/MemoryGame.js
@@ -8,68 +8,74 @@ const cardsCount = 12;
 
 
 function flipCard() {
-  if (lockBoard) return;
-  if (this === firstCard) return;
+    if (lockBoard) return;
+    if (this === firstCard) return;
 
-  this.classList.add("flip");
+    this.classList.add("flip");
 
-  if (!hasFlippedCard) {
-    hasFlippedCard = true;
-    firstCard = this;
-    return;
-  }
+    if (!hasFlippedCard) {
+        hasFlippedCard = true;
+        firstCard = this;
+        return;
+    }
 
-  secondCard = this;
+    secondCard = this;
 
-  checkForMatch();
+    checkForMatch();
 }
 
 function checkForMatch() {
-  let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
-  isMatch ? disableCards() : unflipCard();
+    let isMatch = firstCard.dataset.framework === secondCard.dataset.framework;
+    isMatch ? disableCards() : unflipCard();
 
-  if(flippedCount === cardsCount) {
-    gameEnd();
-  }
+    if (flippedCount === cardsCount) {
+        gameEnd();
+    }
 }
 
 function disableCards() {
-  firstCard.removeEventListener("click", flipCard);
-  secondCard.removeEventListener("click", flipCard);
-  resetBoard();
+    firstCard.removeEventListener("click", flipCard);
+    secondCard.removeEventListener("click", flipCard);
+    resetBoard();
 
-  flippedCount += 2;
+    flippedCount += 2;
 }
 
 function unflipCard() {
-  lockBoard = true;
+    lockBoard = true;
 
-  setTimeout(() => {
-    firstCard.classList.remove("flip");
-    secondCard.classList.remove("flip");
+    setTimeout(() => {
+        firstCard.classList.remove("flip");
+        secondCard.classList.remove("flip");
 
-    resetBoard();
-  }, 750);
+        resetBoard();
+    }, 750);
 }
 
 function resetBoard() {
-  [hasFlippedCard, lockBoard] = [false, false];
-  [firstCard, secondCard] = [null, null];
+    [hasFlippedCard, lockBoard] = [false, false];
+    [firstCard, secondCard] = [null, null];
 
 }
 
 (function shuffle() {
-  cards.forEach((card) => {
-    card.style.order = Math.floor(Math.random() * 12);
-  });
+    cards.forEach((card) => {
+        card.style.order = Math.floor(Math.random() * 12);
+    });
 })();
 
 function gameEnd() {
-  stopTimer();
-  setTimeout(() => alert("Done!"), 500);
-  console.log("Memory Game Done");
+    stopTimer();
+    setTimeout(() => alert("Done!"), 500);
+    console.log("Memory Game Done");
 
-  // disabling start button
-  document.querySelector(".start-button").disabled = true;
-  newRecord();
+    // disabling start button
+    document.querySelector(".watch-button").disabled = true;
+    newRecord();
+}
+
+function flipBoard() {
+    resetBoard();
+    document.querySelectorAll(".memory-card").forEach((element) =>
+        element.classList.remove("flip"));
 }
